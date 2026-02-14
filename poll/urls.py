b2bfb99413241f0556e5348288a748a7c8aff134
Pankaj Kumawat <pankajkumawa2021@gmail.com>
2026-02-14 15:47:09 +0530

    Add files via upload

---
+++ poll/urls.py
@@ -0,0 +1,13 @@
+# urls.py
+from django.contrib import admin
+from django.urls import path
+
+from poll import views as poll_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', poll_views.home, name='home'),
+    path('create/', poll_views.create, name='create'),
+    path('results/<poll_id>/', poll_views.results, name='results'),
+    path('vote/<poll_id>/', poll_views.vote, name='vote'),
+]
